bc976f79765bb6cbdfe1521dd2c283fda6d54a85
nabendu82 <nabendu.biswas@gmail.com>
2022-03-15 12:23:32 +0530

    Day 9

---
+++ DOMBasics/main.js
@@ -0,0 +1,57 @@
+//Grab Single element
+// const form = document.getElementById('my-form');
+// console.log(form);
+
+// console.log(document.querySelector('.container'));
+// console.log(document.querySelector('h1'));
+
+//Select Multiple Item
+// const nodeItems = document.querySelectorAll('.item');
+// const colItems = document.getElementsByClassName('item');
+// console.log(document.getElementsByTagName('li'));
+
+// nodeItems.forEach(item => console.log(item));
+// colItems.forEach(item => console.log(item));
+// for(let i=0; i<colItems.length; i++){
+//     console.log(colItems[i]);
+// }
+
+//DOM Manipulation
+// const ul = document.querySelector('.items');
+// ul.remove();
+
+// const btn = document.querySelector('.btn');
+// btn.style.backgroundColor = 'red';
+// btn.style.textTransform = 'uppercase';
+
+// btn.addEventListener('click', e => {
+//     e.preventDefault();
+//     ul.firstElementChild.textContent = 'HTML';
+//     ul.children[1].innerText = 'CSS';
+//     ul.lastElementChild.innerHTML = '<h3>JavaScript</h3>';
+// })
+
+//Form Manipulation
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerText = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        const text = document.createTextNode(`${nameInput.value}: ${emailInput.value}`);
+        li.appendChild(text);
+        userList.appendChild(li); 
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
