f20c5c895d809fd3973aa9de6f7a4b1ca36426ba
nabendu82 <nabendu.biswas@gmail.com>
2022-03-17 12:03:25 +0530

    Day 10

---
+++ AddressBook/main.js
@@ -0,0 +1,10 @@
+document.getElementById('filterInput').addEventListener('keyup', () => {
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    let ul = document.getElementById('names');
+    let li = ul.querySelectorAll('li.collection-item');
+    li.forEach(item => {
+        let a = item.getElementsByTagName('a')[0];
+        console.log(a.innerText);
+        item.style.display = a.innerText.toUpperCase().startsWith(filterValue) ? 'block' : 'none';
+    })
+})
